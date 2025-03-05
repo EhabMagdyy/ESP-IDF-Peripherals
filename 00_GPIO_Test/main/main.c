ae07b5975bf164f41b00b6400a8874eab9e06431
EhabMagdyy <ehabmagdy0044@gmail.com>
2025-03-06 01:26:20 +0200

    ESP32 DIO Example using LED & Button

--- 00_GPIO_Test/main/main.c
+++ 00_GPIO_Test/main/main.c
@@ -1,44 +1,54 @@
+/***************************************************************************************************************
+*****************************    Author  : Ehab Magdy Abdullah                      ****************************
+*****************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  ****************************
+*****************************    Youtube : https://www.youtube.com/@EhabMagdyy      ****************************
+****************************************************************************************************************/
 
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
 
 void Led_Task(void *pvParameter);
 void Button_Task(void *pvParameter);
 
-#define LED_GPIO        GPIO_NUM_2
-#define BUTTON_GPIO     GPIO_NUM_25
+#define TAG             "DIO Example"
+#define LED_PIN         GPIO_NUM_2
+#define BUTTON_PIN      GPIO_NUM_25
 
 void app_main(void)
 {
-    printf("ESP32 LED & Button Example!\n");
+    ESP_LOGI(TAG, "ESP32 LED & Button Example!");
     xTaskCreate(&Led_Task, "Led_Task", 1024, NULL, 5, NULL);
     xTaskCreate(&Button_Task, "Button_Task", 1024, NULL, 5, NULL);
 }
 
 void Led_Task(void *pvParameter)
 {
-    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
     for(;;)
     {
-        gpio_set_level(LED_GPIO, 1);
+        gpio_set_level(LED_PIN, 1);
         vTaskDelay(pdMS_TO_TICKS(500));
 
-        gpio_set_level(LED_GPIO, 0);
+        gpio_set_level(LED_PIN, 0);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
 void Button_Task(void *pvParameter)
 {
-    gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(BUTTON_GPIO, GPIO_PULLDOWN_ONLY);
+    gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BUTTON_PIN, GPIO_PULLDOWN_ONLY);
 
     for(;;)
     {
-        gpio_get_level(BUTTON_GPIO) ? printf("Button is pressed\n") : printf("Button is not pressed\n");
+        if(gpio_get_level(BUTTON_PIN))
+            ESP_LOGI(TAG, "Button is pressed");
+        else
+            ESP_LOGI(TAG, "Button is not pressed");
+
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
